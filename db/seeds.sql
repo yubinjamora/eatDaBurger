be2a70d8b808e03629ea795f2e3c9c9ad74deecc
Yubin Jamora <yubinjamora@Yubins-MacBook-Pro.local>
2017-12-09 14:24:34 -0500

    <why>

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamber', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheesburger', FALSE);
