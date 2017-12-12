78b14b24838ae0e77940ad6708cc8190e6946f32
Yubin Jamora <yubinjamora@Yubins-MacBook-Pro.local>
2017-12-12 18:28:26 -0500

    updates

--- config/orm.js
+++ config/orm.js
@@ -11,7 +11,7 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
-  // column1=value, column2=value2,...
+ 
   var arr = [];
 
   for (var key in ob) {
@@ -31,8 +31,7 @@ var orm = {
       cb(result);
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+  
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -52,8 +51,7 @@ var orm = {
       cb(result);
     });
   },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
+  
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
